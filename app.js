f50048585b9e255ecfa2b9fb57e5f8c3ade6196e
programators <programatorseu@gmail.com>
2022-12-04 06:40:52 +0100

    3 Nestd components

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            plans: ["Simple", "Medium", "Hard Core"]
-        }
-    }
+    
 })
 .component('plan', {
     template: '#plan-template',
     props: {
         name: {type: String, required: true}
+    }
+})
+.component('plan-picker', {
+    template: '#plan-picker-template',
+    data() {
+        return {
+            plans: ["Simple", "Medium", "Hard Core"]
+        }
     }
 })
 .mount('#app')
